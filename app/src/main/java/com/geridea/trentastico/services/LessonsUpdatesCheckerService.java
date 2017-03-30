6461c41e7940968098147e2ab292c184751d805f
Slava <slavarublev@gmail.com>
2017-03-30 17:10:14 +0200

    Partial commit.

--- app/src/main/java/com/geridea/trentastico/services/LessonsUpdatesCheckerService.java
+++ app/src/main/java/com/geridea/trentastico/services/LessonsUpdatesCheckerService.java
@@ -31,16 +31,32 @@ import com.geridea.trentastico.utils.ContextUtils;
 import com.geridea.trentastico.utils.UIUtils;
 import com.geridea.trentastico.utils.time.CalendarUtils;
 import com.geridea.trentastico.utils.time.WeekInterval;
-import com.threerings.signals.Listener0;
-import com.threerings.signals.Signal0;
+import com.threerings.signals.Listener1;
+import com.threerings.signals.Signal1;
 
 import java.util.Calendar;
 
 public class LessonsUpdatesCheckerService extends Service {
 
+    public static final String EXTRA_STARTER = "EXTRA_STARTER";
+
+    public static final int STARTER_UNKNOWN = 0;
+    public static final int STARTER_NETWORK_BROADCAST = 1;
+    public static final int STARTER_BOOT_BROADCAST = 2;
+    public static final int STARTER_APP_START = 3;
+    public static final int STARTER_ALARM_MANAGER = 4;
+
+
+    public static final int RESCHEDULING_NO = -1;
+    public static final int RESCHEDULING_YES_NORMAL = -2;
+    public static final int RESCHEDULING_YES_QUICK = -3;
+
+
     private JobManager jobManager;
 
     private boolean updateAlreadyInProgress = false;
+
+    private int needsRescheduling = RESCHEDULING_NO;
 
     @Override
     public void onCreate() {
@@ -58,22 +74,73 @@ public class LessonsUpdatesCheckerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(!updateAlreadyInProgress){
-            updateAlreadyInProgress = true;
+        //Check for lessons updates if
+        // 1) More than 4 hours have passed since last check
+        // 2) In the last check we had no internet and now we have it
 
-            long lastUpdate = AppPreferences.getLastLessonsUpdateTime();
-            if (lastUpdate <= getLastPermittedNotUpdatePeriod()) {
-                showToastIfInDebug("Checking for lessons updates...");
-                updateLessons();
-            } else {
-                showToastIfInDebug("Too early to check for updates.");
-            }
+
+        //Rescheduling of the lessons
+        // 1) The check has been successful:
+        //    Just reschedule at the normal pace
+        // 2) We had no internet:
+        //    No reason to reschedule the check; we just wait for the connectivity change broadcast
+        //    and start rescheduling from that.
+        // 3) The check was unsuccessful:
+        //    Reschedule at a slower rate.
+
+        if (updateAlreadyInProgress) {
+            //The service is already started and it's doing something
+            showToastIfInDebug("Update already in progress... ignoring update.");
+            return START_REDELIVER_INTENT;
         } else {
-            showToastOnMainThread("Update already in progress.. ignoring update.");
+            updateAlreadyInProgress = true;
+        }
+
+        int starter = intent.getIntExtra(EXTRA_STARTER, STARTER_UNKNOWN);
+        if(shouldUpdateBecauseWeGainedInternet(starter)){
+            showToastIfInDebug("Updating because of internet refresh state.");
+
+            updateLessons(new LessonsUpdateListener() {
+                @Override
+                public void onLessonsUpdateTerminated(boolean successful) {
+
+
+                }
+
+                @Override
+                public void onNoInternet() {
+
+                }
+            });
+
+        } else if (shouldUpdateBecauseOfUpdateTimeout()) {
+            showToastIfInDebug("Checking for lessons updates...");
+            updateLessons(starter);
+        } else
+
+        updateAlreadyInProgress = true;
+
+        long lastUpdate = AppPreferences.getLastLessonsUpdateTime();
+        if (lastUpdate <= getLastPermittedNotUpdatePeriod()) {
+            showToastIfInDebug("Checking for lessons updates...");
+            updateLessons(starter);
+        } else {
+            showToastIfInDebug("Too early to check for updates.");
         }
 
         return START_REDELIVER_INTENT;
     }
+
+    private boolean shouldUpdateBecauseWeGainedInternet(int starter) {
+        return starter == STARTER_NETWORK_BROADCAST
+            && !AppPreferences.hadInternetInTheLastLessonsUpdate()
+            && ContextUtils.weHaveInternet(this);
+    }
+
+    private boolean shouldUpdateBecauseOfUpdateTimeout() {
+        return AppPreferences.getLastLessonsUpdateTime() <= getLastPermittedNotUpdatePeriod();
+    }
+
 
     private void showToastIfInDebug(String message) {
         if (Config.DEBUG_MODE) {
@@ -81,19 +148,19 @@ public class LessonsUpdatesCheckerService extends Service {
         }
     }
 
-    private void scheduleNextStartAndStop(boolean halfTime) {
+    private void scheduleNextStartAndTerminate(boolean halfTime) {
         long lastUpdate = AppPreferences.getLastLessonsUpdateTime();
         Calendar calendar = CalendarUtils.getCalendarInitializedAs(lastUpdate);
 
-        if(Config.DEBUG_MODE) {
+        if (Config.DEBUG_MODE) {
             int timeToAdd = Config.DEBUG_LESSONS_REFRESH_WAITING_RATE_SECONDS;
             if (halfTime) timeToAdd /= 2;
-            
+
             calendar.add(Calendar.SECOND, timeToAdd);
         } else {
             int timeToAdd = Config.LESSONS_REFRESH_WAITING_HOURS;
             if (halfTime) timeToAdd /= 2;
-            
+
             calendar.add(Calendar.HOUR_OF_DAY, timeToAdd);
         }
 
@@ -107,30 +174,33 @@ public class LessonsUpdatesCheckerService extends Service {
         stopSelf();
     }
 
-    private void updateLessons() {
-        if(AppPreferences.isStudyCourseSet() && ContextUtils.weHaveInternet(this)){
-            UpdateLessonsJob job = new UpdateLessonsJob();
-            job.onRequestTerminatedSuccessfully.connect(new Listener0() {
-                @Override
-                public void apply() {
-                    showToastIfInDebug("Lesson update successful!");
-                    updateLastUpdateTime();
-                    scheduleNextStartAndStop(false);
-                }
-            });
-            job.onRequestTerminatedWithErrors.connect(new Listener0() {
-                @Override
-                public void apply() {
-                    showToastIfInDebug("Lesson update happened with error!");
-                    scheduleNextStartAndStop(true);
-                }
-            });
-            jobManager.addJobInBackground(job);
+    private void updateLessons(final LessonsUpdateListener listener) {
+        if (ContextUtils.weHaveInternet(this)) {
+            if (AppPreferences.isStudyCourseSet()) {
+                UpdateLessonsJob job = new UpdateLessonsJob();
+                job.onCheckTerminated.connect(new Listener1<Boolean>() {
+                    @Override
+                    public void apply(Boolean isSuccessful) {
+                        updateLastUpdateTime();
+                        if (isSuccessful) {
+                            showToastIfInDebug("Lesson update successful!");
+                        } else {
+                            showToastIfInDebug("Lesson update happened with error!");
+                        }
+
+                        listener.onLessonsUpdateTerminated(isSuccessful);
+                    }
+                });
+                jobManager.addJobInBackground(job);
+            } else {
+                //The user has just run the app or reset it's settings. We currently do not have any
+                //study course to fetch lessons from, so we just re-plan the check to the next time.
+                updateLastUpdateTime();
+                listener.onLessonsUpdateTerminated(false);
+            }
         } else {
-            //The user has just run the app or reset it's settings. We currently do not have any
-            //study course to fetch lessons from, so we just re-plan the check to the next time.
-            updateLastUpdateTime();
-            scheduleNextStartAndStop(true);
+            showToastIfInDebug("No internet. Cannot check for updates.");
+            listener.onNoInternet();
         }
     }
 
@@ -158,22 +228,21 @@ public class LessonsUpdatesCheckerService extends Service {
 
     private class UpdateLessonsJob extends Job implements LessonsLoadingListener {
 
-        final Signal0 onRequestTerminatedSuccessfully = new Signal0();
-
-        final Signal0 onRequestTerminatedWithErrors = new Signal0();
+        final Signal1<Boolean> onCheckTerminated = new Signal1<>();
 
         private int numRequestsSent, numRequestsSucceeded, numRequestsFailed;
 
         UpdateLessonsJob() {
             super(new Params(1));
 
-            numRequestsSent      = 0;
+            numRequestsSent = 0;
             numRequestsSucceeded = 0;
-            numRequestsFailed    = 0;
+            numRequestsFailed = 0;
         }
 
         @Override
-        public void onAdded() { }
+        public void onAdded() {
+        }
 
         @Override
         public void onRun() throws Throwable {
@@ -183,7 +252,8 @@ public class LessonsUpdatesCheckerService extends Service {
         }
 
         @Override
-        protected void onCancel(int cancelReason, @Nullable Throwable throwable) { }
+        protected void onCancel(int cancelReason, @Nullable Throwable throwable) {
+        }
 
         @Override
         protected RetryConstraint shouldReRunOnThrowable(@NonNull Throwable throwable, int runCount, int maxRunCount) {
@@ -234,14 +304,19 @@ public class LessonsUpdatesCheckerService extends Service {
 
         private void checkIfWeHaveFinished() {
             if (numRequestsSent == (numRequestsSucceeded + numRequestsFailed)) {
-                if (numRequestsSent == numRequestsSucceeded){
-                    onRequestTerminatedSuccessfully.dispatch();
+                if (numRequestsSent == numRequestsSucceeded) {
+                    onCheckTerminated.dispatch(true);
                 } else {
-                    onRequestTerminatedWithErrors.dispatch();
+                    onCheckTerminated.dispatch(false);
                 }
             }
         }
 
     }
 
+    private interface LessonsUpdateListener {
+        void onLessonsUpdateTerminated(boolean successful);
+
+        void onNoInternet();
+    }
 }
