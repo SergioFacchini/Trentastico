6aeba760834e7b3221cf5264055d6b4a2c8fe5f6
slava <slavarublev@gmail.com>
2020-01-18 17:13:55 +0100

    Disabling donation dialog before RoomTick notification arrives

--- app/src/main/java/com/geridea/trentastico/services/DonationPopupManager.kt
+++ app/src/main/java/com/geridea/trentastico/services/DonationPopupManager.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.geridea.trentastico.utils.AppPreferences
 import com.geridea.trentastico.utils.ContextUtils
 import com.geridea.trentastico.utils.time.CalendarUtils
+import java.util.*
 
 class DonationPopupManager {
 
@@ -14,10 +15,21 @@ class DonationPopupManager {
             AppPreferences.nextDonationNotificationDate = nextSchedule
         }
 
-        fun shouldPopupBeShown(context: Context): Boolean =
-                AppPreferences.showDonationPopups &&
-                AppPreferences.nextDonationNotificationDate <= System.currentTimeMillis() &&
-                        ContextUtils.weHaveInternet(context)
+        fun shouldPopupBeShown(context: Context): Boolean {
+            // Do not show the donation dialog dialog until the RoomTick notification arrived
+            val roomTickReleaseDay = Calendar.getInstance()
+            roomTickReleaseDay.set(2020, Calendar.JANUARY, 25, 23, 59)
+
+            val roomTickReleaseDayMillis = roomTickReleaseDay.timeInMillis
+            val now = System.currentTimeMillis()
+            if (now <= roomTickReleaseDayMillis) {
+                return false
+            }
+
+            return AppPreferences.showDonationPopups &&
+                    AppPreferences.nextDonationNotificationDate <= System.currentTimeMillis() &&
+                    ContextUtils.weHaveInternet(context)
+        }
 
         fun rescheduleNotification() {
             val nextSchedule = CalendarUtils.addDays(System.currentTimeMillis(), 5)
