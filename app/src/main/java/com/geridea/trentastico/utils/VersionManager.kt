ab4315090f4590f3dcc979d498967332e08b8d28
Laurence Bonat <laurence.bonat@gmail.com>
2024-08-27 15:47:56 +0200

    feat: Aggiornato anno accademico

--- app/src/main/java/com/geridea/trentastico/utils/VersionManager.kt
+++ app/src/main/java/com/geridea/trentastico/utils/VersionManager.kt
@@ -132,6 +132,16 @@ object VersionManager {
             AppPreferences.hasToUpdateStudyCourse = true
         }
 
+        if (lastVersion in 1..35) {
+            //The academic year has changed: 2024 - 2025
+            Networker.obliterateCache()
+            AppPreferences.clearLessonsToHide()
+            AppPreferences.removeAllExtraCourses()
+            AppPreferences.removeStudyCourse()
+
+            AppPreferences.hasToUpdateStudyCourse = true
+        }
+
         AppPreferences.lastVersionExecuted = thisVersion
     }
 
